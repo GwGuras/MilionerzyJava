c34181612d84ceb969dff401806acd36088f8bb5
Szczepan Wójcik <34340@student.pwsztar.edu.pl>
2022-05-05 21:40:50 +0200

    Remove Java 9 JPMS modular nature of project

--- Milionerzy/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module pl.edu.anstar.milionerzy {
-	requires transitive javafx.graphics;
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens pl.edu.anstar.milionerzy to javafx.fxml;
-    exports pl.edu.anstar.milionerzy;
-}
