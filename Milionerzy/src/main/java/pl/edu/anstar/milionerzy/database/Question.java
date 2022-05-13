3e3bc111e73d302c4e0cc9fbcb1ac06ca7badf89
Szczepan Wójcik <34340@student.pwsztar.edu.pl>
2022-05-13 14:03:15 +0200

    Moved database query to Question class. Added database asset.

--- Milionerzy/src/main/java/pl/edu/anstar/milionerzy/database/Question.java
+++ Milionerzy/src/main/java/pl/edu/anstar/milionerzy/database/Question.java
@@ -1,5 +1,8 @@
 package pl.edu.anstar.milionerzy.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -55,6 +58,32 @@ public class Question {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static Question getRandomQuestion(int prize) {
+		Connection connection = null;
+		try {
+			StringBuilder sB = new StringBuilder("jdbc:sqlite:");
+			sB.append(System.getProperty("user.dir")); // project location
+			sB.append("/resources/assets/Milionerzy.db");
+			connection = DriverManager.getConnection(sB.toString());
+			PreparedStatement statement = connection
+					.prepareStatement("SELECT question,answer1,answer2,answer3,answer4,correct"
+							+ " FROM Quesitons WHERE prize=" + prize + " ORDER BY random() LIMIT 1;");
+			ResultSet results = statement.executeQuery();
+			return new Question(results, prize);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (connection != null) {
+					connection.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
 	}
 	
 }
